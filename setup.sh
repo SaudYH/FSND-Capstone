871a98d88a7512cdc485d34cc563968ea1df2de1
Saud <saud.yahya.h@gmail.com>
2024-02-23 14:34:27 +0300

    push to heroku

--- setup.sh
+++ setup.sh
@@ -3,3 +3,6 @@ export AUTH0_CLIENT_SECRET=SAcbbgmKpaSasPoRfSw1PcITYT8euF5XBiwH0wCQAb1sVcGsnqqqc
 export AUTH0_DOMAIN=dev-lcdbke2zg51u1eut.us.auth0.com
 export APP_SECRET_KEY=ALongRandomlyGenerated
 export AUTH0_AUDIENCE="http://127.0.0.1:5000/actors"
+export DATABASE_URL="postgresql://postgres@localhost:5432/agency"
+export EXCITED="true"
+echo "setup.sh script executed successfully!"
