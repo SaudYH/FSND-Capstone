5135e3c60f55a333c9e2452aa98c584cd4950ce3
Saud <saud.yahya.h@gmail.com>
2024-02-23 14:38:55 +0300

    use env variable for DB

--- models.py
+++ models.py
@@ -1,16 +1,14 @@
 import enum
+import os
 from sqlalchemy import Column, String, Integer, DATE, Enum
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
 from flask_migrate import Migrate
 
-database_name = "agency"
-database_path = "postgresql://{}/{}".format('127.0.0.1:5432', database_name)
-
 db = SQLAlchemy()
 
 
-def setup_db(app, database_path=database_path):
+def setup_db(app, database_path=os.environ['DATABASE_URL']):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
     migrate = Migrate(app, db)
